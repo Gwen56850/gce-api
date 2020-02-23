634b0fc477a7646e9526985be8a7c2eceae602f4
Gwendal Le Cren <gwendal.le-cren@revevol.eu>
2020-02-23 14:55:55 +0100

    Init GCE api

---
+++ src/main/java/com/glc/snippet/endpoint/UserEndpoint.java
@@ -0,0 +1,22 @@
+package com.glc.snippet.endpoint;
+
+import com.glc.snippet.model.AppUser;
+import com.google.api.server.spi.ServiceException;
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.config.ApiClass;
+import com.google.api.server.spi.config.ApiMethod;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.google.api.server.spi.config.ApiMethod.HttpMethod.GET;
+
+@ApiClass(resource = "users")
+@Slf4j
+public class UserEndpoint extends AuthenticatedEndpoint {
+
+    @ApiMethod(path = "me", httpMethod = GET)
+    public AppUser getCurrentUser(User user) throws ServiceException {
+        log.info("User email {} called the api", user.getEmail());
+        return wrap(user, googleUser -> AppUser.create(user));
+    }
+
+}
